4e929da7e7722444a0f27470b85141cc55af7264
Leonhardt Koepsell <leonhardt@koepsell.io>
2021-04-18 09:41:13 -0700

    Fix entrypoint setup script

--- pipeline/images/k8s-gcp/entrypoint.sh
+++ pipeline/images/k8s-gcp/entrypoint.sh
@@ -14,12 +14,12 @@ ensure_env_var_set KUBECONFIG_FILE
 ensure_env_var_set GCP_SERVICE_ACCOUNT_FILE
 ensure_env_var_set CONTEXT
 
-kubectl config use-context $CONTEXT
 mkdir -p .config
 printenv KUBECONFIG_FILE > .config/kubeconfig
 printenv GCP_SERVICE_ACCOUNT_FILE > .config/gcp-service-account.json
 export KUBECONFIG=$(realpath .config/kubeconfig)
 export GOOGLE_APPLICATION_CREDENTIALS=$(realpath .config/gcp-service-account.json)
+kubectl config use-context $CONTEXT
 
 if [[ "$#" != "0" ]]; then
   "$@"
