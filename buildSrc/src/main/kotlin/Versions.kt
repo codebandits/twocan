727eda12158d79fb0e55285396f6ad04f452c479
Leonhardt Koepsell <leonhardt@koepsell.io>
2021-08-12 23:27:24 -0700

    Upgrade server dependencies

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,10 +1,10 @@
 object Versions {
     const val Kotlin = "1.5.21"
     const val JvmTarget = "15"
-    const val Http4k = "4.10.0.0.0"
+    const val Http4k = "4.11.0.1"
     const val Shadow = "7.0.0"
     const val Logback = "1.3.0-alpha5"
-    const val Sl4fj = "2.0.0-alpha2"
+    const val Sl4fj = "2.0.0-alpha3"
     const val KotlinLogging = "2.0.10"
     const val HamKrest = "1.8.0.1"
     const val JsonPath = "2.6.0"
